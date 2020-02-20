e0dbe63bec6f82eb76e8bed0a5fa82e6704c3769
BARISU <m-ishii-6z5@eagle.sophia.ac.jp>
2020-02-20 10:15:54 +0900

    プロジェクト一覧折り畳み

--- web/web/static/projects.js
+++ web/web/static/projects.js
@@ -4,6 +4,7 @@ const ProjectList = {
   data() {
     return {
       project_lists: [],
+      isShow:{}
     }
   },
   template: `
@@ -11,12 +12,14 @@ const ProjectList = {
     <h1>ProjectList</h1>
     <ul>
         <li v-for="project in project_lists">
-              プロジェクト名: {{ project.title }}<br>
-              説明: {{ project.description }} <br>
-              会計種別: {{ project.accounting_type }} <br>
-              完了フラグ: {{ project.closed }} <br>
-              承認済予算: {{ project.sum_budget }} <br>
-              支出済予算: {{ project.sum_purchase_price }} <br>
+              <p v-on:click="show(project.title)" style="cursor:pointer">プロジェクト名: {{ project.title }}</p>
+              <div v-show="isShow[project.title]">
+                説明: {{ project.description }} <br>
+                会計種別: {{ project.accounting_type }} <br>
+                完了フラグ: {{ project.closed }} <br>
+                承認済予算: {{ project.sum_budget }} <br>
+                支出済予算: {{ project.sum_purchase_price }} <br>
+              </div>
         </li>
     </ul>
   </div>
@@ -25,11 +28,21 @@ const ProjectList = {
     api.get('/v1/api/projects/')
       .then(response => {
         this.project_lists = response.data;
+        for (let cnt = 0;cnt < response.data.length;cnt ++ ){
+          this.$set(this.isShow,response.data[cnt].title,false);
+        }
+        console.log(this.isShow);
       })
       .catch(error => {
         console.log(error);
       });
+  },
+  methods:{
+    show: function( title ){
+      this.$set(this.isShow,title,!this.isShow[title]);
+    }
   }
+
 };
 
 export { ProjectList };
