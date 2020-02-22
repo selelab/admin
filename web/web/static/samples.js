5b6eef838c7d701c49ea23b1557942f0d7af5526
Miyu Sakuma <miyusakuma@Miyus-MacBook-Air-2.local>
2020-02-23 00:23:31 +0900

    sshiz

--- web/web/static/samples.js
+++ web/web/static/samples.js
@@ -6,28 +6,33 @@ const SampleList  = {
   },
   template:`
   <div>
-     <h1>Sample List</h1>
-     <router-link to="/samples/1">
-       初めてのVue.js
-     </router-link> <br>
-     <router-link to="/samples/2">
-       v-bindとmethod
-     </router-link> <br>
+    <div>
+        <h1>Sample List</h1>
+        <router-link to="/samples/1">
+            初めてのVue.js
+        </router-link> <br>
+        <router-link to="/samples/2">
+            v-bindとmethod
+        </router-link> <br>
+    </div>
+
+    &nbsp
+    &nbsp
+    &nbsp
+
+    <div class="modal_wrap">
+        <input id="trigger" type="checkbox">
+        <div class="modal_overlay">
+            <label for="trigger" class="modal_trigger">shit</label>
+            <div class="modal_content">
+                <label for="trigger" class="close_button">&#x2716;&#xfe0f;</label>
+                <h2>Modal Content</h2>
+                <p>Lorem ipsum ...</p>
+            </div>
+        </div>
+    </div>
+    <label for="trigger" class="open_button">OPEN</label>
   </div>
-
-  <div class="modal_wrap">
-<input id="trigger" type="checkbox">
-	<div class="modal_overlay">
-		<label for="trigger" class="modal_trigger"></label>
-		<div class="modal_content">
-			<label for="trigger" class="close_button">&#x2716;&#xfe0f;</label>
-			<h2>Modal Content</h2>
-			<p>Lorem ipsum ...</p>
-		</div>
-	</div>
-</div>
-
-
   `
   ,
 };
