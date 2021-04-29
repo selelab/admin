c0db5a6272c50feda472b0db766a4547f30b5c68
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2021-04-29 20:07:35 +0900

    api.selelab.com経由でアクセスをしないようにした

--- front/src/api.js
+++ front/src/api.js
@@ -1,19 +1,7 @@
 import Axios from 'axios'
 
-Axios.defaults.xsrfHeaderName = 'X-CSRFToken'
-Axios.defaults.xsrfCookieName = 'csrftoken'
-
-
-const getBaseURL = (host) => {
-  if (host === 'selelab.com') {
-    return 'https://api.selelab.com'
-  } else {
-    return ''
-  }
-}
-
 const api = Axios.create({
-  baseURL: `${getBaseURL(location.host)}/api/admin`,
+  baseURL: "/api/admin",
   headers: {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest'
