350e79b6cfb993ee79ea8629719444705bd11160
tomtsutom <40418321+Tsutomu-Ikeda@users.noreply.github.com>
2021-08-25 23:12:02 +0900

    プロジェクトが作成されたときにRedisに対してpublishを行うようにした

--- api/constants.py
+++ api/constants.py
@@ -3,3 +3,8 @@ import environ
 env = environ.Env()
 
 S3_STATIC_BUCKET_NAME = 'selelab-static'
+
+REDIS_HOST = env.str('REDIS_HOST')
+REDIS_PORT = env.str('REDIS_PORT')
+REDIS_DB = env.str('REDIS_DB')
+REDIS_PASS = env.str('REDIS_PASS')
